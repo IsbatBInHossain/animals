9081d93ff023fc007ca31e216dd27f1c54819575
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-03-22 16:48:45 +0600

    Add functionality and Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-export const App = () => {
+import AnimalShow from "./AnimalShow";
+import "./App.css";
+import { useState } from "react";
+
+const getRandomAnimal = () => {
+  const animals = ["cat", "dog", "gator", "cow", "bird", "horse"];
+  let index = Math.floor(Math.random() * animals.length);
+  return animals[index];
+};
+
+const App = () => {
+  const [animals, setAnimals] = useState([]);
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+  const renderAnimal = animals.map((animal, index) => (
+    <AnimalShow type={animal} key={index} />
+  ));
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <button className="btn" onClick={handleClick}>
+        Add Animal
+      </button>
+      <div className="render-animals">{renderAnimal}</div>
+    </div>
+  );
+};
+export default App;
